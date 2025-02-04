41cca0047baeed536ca03ce348762a1d8bad6d98
JDOM <juandiegoosorio10@hotmail.com>
2025-02-04 14:49:34 -0500

    chore: initialize models with Vehicle and restriction rules

---
+++ PicoYPlaca/Models/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoYPlaca.Models
+{
+    public class Vehicle
+    {
+        private string plate;
+        private int lastDigit;
+
+        public Vehicle(string plate)
+        {
+            this.plate = plate;
+            this.lastDigit = int.Parse(plate.Substring(plate.Length - 1));
+        }
+
+        public string Plate
+        {
+            get { return this.plate; }
+            set { this.plate = value; }
+        }
+
+        public int LastDigit
+        {
+            get { return this.lastDigit; }
+        }
+
+
+    }
+}
---
+++ PicoYPlaca/Rules/Restrictions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoYPlaca.Rules
+{
+    public static class Restrictions
+    {
+        public static readonly string[][] RestrictedHours =
+        {
+            ["07:00", "09:30"],
+            ["16:00", "19:30"]
+        };
+
+        public static readonly Dictionary<DayOfWeek, int[]> RestrictionRules = new()
+        {
+            { DayOfWeek.Monday, new[] { 1, 2 } },
+            { DayOfWeek.Tuesday, new[] { 3, 4 } },
+            { DayOfWeek.Wednesday, new[] { 5, 6 } },
+            { DayOfWeek.Thursday, new[] { 7, 8 } },
+            { DayOfWeek.Friday, new[] { 9, 0 } }
+        };
+    }
+}
